f77acff5bbe0e91a87568edc9abcc8541a8dfc08
Maurício Da Silva Pires <mauricio.spires@yahoo.com.br>
2016-12-08 21:35:37 -0200

    Update trab_abm.c

--- trab_abm.c
+++ trab_abm.c
@@ -1,4 +1,21 @@
 //diretivas de compilação
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct arvbm {
+  int *chave , nchaves, folha;
+  struct reg **info;
+  struct arvbm **filho, *prox;
+} TABM;
+
+typedef struct reg {
+  int mat;
+  float cr;
+  int tranc;
+  int ch_aprov, periodos, cur;
+  char *nome;
+} TREG;
+  
 
 //funções a serem implementadas
 
