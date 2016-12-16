e6859602c0e3c81a6d4d646dca451a7760df561b
Maurício Da Silva Pires <mauricio.spires@yahoo.com.br>
2016-12-16 18:37:50 -0200

    Update trab_abm.c

--- trab_abm.c
+++ trab_abm.c
@@ -4,7 +4,7 @@
 
 typedef struct arvbm {
   int *chave , nchaves, folha;
-  struct reg **info;
+  struct reg **info;                //vetor de ponteiros para as informações
   struct arvbm **filho, *prox;
 } TABM;
 
@@ -12,12 +12,24 @@ typedef struct reg {
   int mat;
   float cr;
   int tranc;
-  int ch_aprov, periodos, cur;
+  int ch_aprov, periodos, cur;    //carga horária com aprovação, número de períodos e currículo.
   char *nome;
 } TREG;
   
+TABM *Cria(int t){
+  TAB* novo = (TAB *) malloc (sizeof(TAB));
+  novo->nchaves = 0;
+  novo->chave = (int *) malloc (sizeof(int*)*((t*2)-1));
+  novo->folha = 1;
+  novo->info = (TREG **) malloc (sizeof(TREG*)*((t*2)-1));
+  novo->filho = (TAB **) malloc (sizeof(TAB*)*t*2);
+  novo->prox = (TABM *) malloc (sizeof(TABM*));
+  int i;
+  for(i=0; i<(t*2); i++) novo->filho[i] = NULL;
+  return novo;
+}
 
-//funções a serem implementadas
+
 
 int main (int argc, char** argv) {
 
