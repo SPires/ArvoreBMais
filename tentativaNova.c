1a3914a92aa644a81906018a518c2b5635aeb6d3
Maurício Da Silva Pires <mauricio.spires@yahoo.com.br>
2016-12-30 20:10:17 -0200

    Função gravarDados reduzida

--- tentativaNova.c
+++ tentativaNova.c
@@ -549,26 +549,18 @@ TABM * novaArv (char *nome, int t){
  *
  * retorno: nova árvore */
 int gravarDados (TABM *a, char *saida){
-  if (!a) return 0;
+  if (!a) {
+     printf("%s","Árvore não existe");
+     return 0;
+  }
   FILE *fp = fopen(saida,"wt+");
   if (!fp) exit(1);
+  if (!a->folha) a = primeiraFolha(a);
   int i;
   TREG *aux = (TREG *) malloc (sizeof(TREG));
-  if (a->folha){
-    for (i=0;i<a->nchaves;i++){
-       aux = a->info[i];
-	fprintf(stderr,"DEBUG: FOLHA %d %f %d %d %d %d %s\n",a->info[i]->mat,a->info[i]->cr,a->info[i]->tranc,a->info[i]->ch_aprov,a->info[i]->periodos,a->info[i]->cur,a->info[i]->nome);
-       fprintf(fp, "%d %f %d %d %d %d %s\n",a->info[i]->mat,a->info[i]->cr,a->info[i]->tranc,a->info[i]->ch_aprov,a->info[i]->periodos,a->info[i]->cur,a->info[i]->nome);
-    }
-	  fclose(fp);
-	  return 1;
-  }
-  
-  a = primeiraFolha(a);
   while (a) {
     for (i=0;i<a->nchaves;i++){
        aux = a->info[i];
-	fprintf(stderr,"DEBUG: NAOFOLHA %d %f %d %d %d %d %s\n",a->info[i]->mat,a->info[i]->cr,a->info[i]->tranc,a->info[i]->ch_aprov,a->info[i]->periodos,a->info[i]->cur,a->info[i]->nome);
        fprintf(fp, "%d %f %d %d %d %d %s\n",aux->mat,aux->cr,aux->tranc,aux->ch_aprov,aux->periodos,aux->cur,aux->nome);
     }
     a = a->prox;
